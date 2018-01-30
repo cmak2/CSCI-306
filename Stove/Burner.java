e7dd53e9f5e7be64d5500e8039d86fa671ea3139
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-01-30 12:18:05 -0700

    Committed Methods.

--- Stove/Burner.java
+++ Stove/Burner.java
@@ -1,8 +1,60 @@
 
+/*
+ * Names:
+ * Calvin Mak
+ * Jacob McBee
+ * Date:
+ * 1/30/2018
+ */
+
 public class Burner {
+	
+	Burner() {							//Initialize Burner Instructions
+		this.myTemperature = Temperature.COLD;
+		this.mySetting	   = Setting.OFF;
+	}
+	
+	public void plusButton() {			//Will Increase Temperature
+		switch(mySetting) {				//Need to set Timer in Button methods
+		case OFF:
+			this.mySetting = Setting.LOW;
+			break;
+		case LOW:
+			this.mySetting = Setting.MEDIUM;
+			break;
+		case MEDIUM:
+			this.mySetting = Setting.HIGH;
+			break;
+		case HIGH:
+			this.mySetting = Setting.HIGH;
+		}
+	}
+	
+	public void minusButton() {
+		switch(mySetting) {
+		case OFF:
+			this.mySetting = Setting.OFF;
+			break;
+		case LOW:
+			this.mySetting = Setting.OFF;
+			break;
+		case MEDIUM:
+			this.mySetting = Setting.LOW;
+			break;
+		case HIGH:
+			this.mySetting = Setting.MEDIUM;
+		}
+	}
+	
+	public void updateTemperature() {
+		
+	}
+	
+	public void display() {
+		mySetting.toString();			//Will Fix later
+	}
+	
 	public enum Temperature { BLAZING, HOT, WARM, COLD};
-	
-	
 
 	public Temperature getMyTemperature() {
 		return myTemperature;
