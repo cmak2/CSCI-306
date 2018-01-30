468043be1d315ca16457bb80a316635a831d153d
mcbeej <35963024+mcbeej@users.noreply.github.com>
2018-01-30 11:48:12 -0700

    Update Setting.java

--- Stove/Setting.java
+++ Stove/Setting.java
@@ -1,1 +1,22 @@
+package src;
+import java.lang.*;
 
+public enum Setting {
+	OFF ("---"), LOW ("--+"), MEDIUM ("-++"), HIGH ("+++");
+	
+	private String heatSetting;
+	
+	Setting(String initSetting){
+		heatSetting = initSetting;
+	}
+
+	public String getHeatSetting() {
+		return heatSetting;
+	}
+
+	public void setHeatSetting(String heatSetting) {
+		this.heatSetting = heatSetting;
+	}
+	
+	
+}
