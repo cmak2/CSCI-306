fd561020311ed2cc7dd8fcf813e6cf16dec39d63
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-01-30 11:54:55 -0700

    Initialized Data Structures

--- Stove/Burner.java
+++ Stove/Burner.java
@@ -1,1 +1,19 @@
 
+public class Burner {
+	public enum Temperature { BLAZING, HOT, WARM, COLD};
+	
+	
+
+	public Temperature getMyTemperature() {
+		return myTemperature;
+	}
+
+	public void setMyTemperature(Temperature myTemperature) {
+		this.myTemperature = myTemperature;
+	}
+	
+	public final int TIME_DURATION = 2;
+	private int timer;
+	private Setting mySetting;
+	private Temperature myTemperature;
+}
