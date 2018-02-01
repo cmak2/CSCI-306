460d6b97eae6a3fc85fafc80b9c0ef0b6cddcd00
mcbeej <35963024+mcbeej@users.noreply.github.com>
2018-02-01 12:14:55 -0700

    Create LevelEngine.java

---
+++ GameEngine/LevelEngine.java
@@ -0,0 +1,45 @@
+package levelPieces;
+
+import java.util.ArrayList;
+
+import gameEngine.Drawable;
+import gameEngine.Moveable;
+
+/**
+ * 
+ * @author Calvin Mak
+ * @author Jacob Mcbee
+ *
+ * Programming pair 23
+ * CSCI 306 SEC A
+ * 
+ */
+
+public class LevelEngine {
+
+	public void createLevel(int levelNum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Drawable[] getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Moveable> getMovingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<GamePiece> getInteractingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPlayerStartLoc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
