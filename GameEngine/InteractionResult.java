a8c25056582fd1b2c96a3ba03c0be59e13b690b0
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-02-01 12:12:13 -0700

    Created File

---
+++ GameEngine/InteractionResult.java
@@ -0,0 +1,13 @@
+package gameEngine;
+
+/**
+ * Interacting with a game piece must yield one of these results.
+ * 
+ * @author Mark Baldwin
+ * @author Cyndi Rader
+ * 
+ */
+public enum InteractionResult {
+	HIT, KILL, ADVANCE, GET_POINT, NONE;
+
+}
