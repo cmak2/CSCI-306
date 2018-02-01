01161d2bab56c1958243b4903f788d82b1b94ba6
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-02-01 12:13:49 -0700

    Created File

---
+++ GameEngine/Drawable.java
@@ -0,0 +1,16 @@
+package gameEngine;
+/*
+* @author Calvin Mak
+* @author Jacob Mcbee
+*/
+/**
+ * All game pieces may be "drawn" - just printed on console, 
+ * no parameters needed.
+ * 
+ * @author Mark Baldwin
+ * @author Cyndi Rader
+ * 
+ */
+public interface Drawable {
+	public void draw();
+}
