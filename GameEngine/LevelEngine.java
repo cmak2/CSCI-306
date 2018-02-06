d55248452169fef2d3a721e721449cb7c8bca1b6
mcbeej <35963024+mcbeej@users.noreply.github.com>
2018-02-06 12:09:37 -0700

    Update LevelEngine.java

--- GameEngine/LevelEngine.java
+++ GameEngine/LevelEngine.java
@@ -3,6 +3,7 @@ package levelPieces;
 import java.util.ArrayList;
 
 import gameEngine.Drawable;
+import gameEngine.GameEngine;
 import gameEngine.Moveable;
 
 /**
@@ -25,12 +26,16 @@ public class LevelEngine {
 		
 		//TODO: levelNum needs a function to differentiate the levels
 		//TODO: set up moveable pieces
-		currentBoard = new Drawable[MAX_BOARD_SIZE];
-		RandomPiece testPiece = new RandomPiece();
-		currentBoard[5] = testPiece;
-		interactingPieces = new ArrayList<GamePiece>(MAX_BOARD_SIZE);
-		interactingPieces.add(testPiece);
-		movingPieces = new ArrayList<Moveable>(MAX_BOARD_SIZE);
+		currentBoard = new Drawable[GameEngine.BOARD_SIZE];
+		interactingPieces = new ArrayList<GamePiece>(GameEngine.BOARD_SIZE);
+		movingPieces = new ArrayList<Moveable>(GameEngine.BOARD_SIZE);
+		
+		for(int i = 0; i < MAX_HEAL_UNITS; i++) {
+			switch()
+		}
+		
+		
+		
 	}
 
 	public Drawable[] getBoard() {
@@ -41,23 +46,33 @@ public class LevelEngine {
 	}
 
 	public ArrayList<Moveable> getMovingPieces() {
-		// TODO Auto-generated method stub
 		return movingPieces;
 	}
 
 	public ArrayList<GamePiece> getInteractingPieces() {
-		// TODO Auto-generated method stub
 		return interactingPieces;
 	}
 
 	public int getPlayerStartLoc() {
-		
-		return 0;
+		return GameEngine.BOARD_SIZE / 2;
 	}
 	
-	public final static int MAX_BOARD_SIZE = 10;
 	private Drawable[] currentBoard;
 	private ArrayList<Moveable> movingPieces;
 	private ArrayList<GamePiece> interactingPieces;
-
+	
+	
+	/*
+	 * The following consts are used to determine the maximum amount of units of a InteractionResult type
+	 * For example for takeDamage class (called ATTACK here), there will be an upper limit of 2 attacking units
+	 * 
+	 */
+	private final int MAX_HEAL_UNITS = 2;
+	private final int MAX_ATTACK_UNITS = 2;
+	private final int MAX_KILL_UNITS = 1;
+	private final int MAX_TRIP_UNITS = 1;
+	private final int MAX_ADVANCE_UNITS = 1;
+	private final int MAX_GET_POINT_UNITS = 2;
+	
+	private final int MAX_DRAW_ONLY_UNITS = 3;
 }
